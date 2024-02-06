afdd62be381439a763c0eef640f75ca0914a4b32
Opilane TTHK <opilane@tthk.ee>
2024-02-06 12:50:23 +0200

    Planetos

---
+++ Planeedid/Planeedid/Services/PlanetsService.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planeedid.Services
+{
+    internal static class PlanetsService
+    {
+        private static List<Planet> planets = new()
+        {
+            new()
+            {
+
+            }
+        };
+    }
+}
