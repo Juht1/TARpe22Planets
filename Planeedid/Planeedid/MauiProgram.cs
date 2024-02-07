d6e16adaf74b3f27419761805e49da37b1b9098f
Opilane TTHK <opilane@tthk.ee>
2024-02-07 11:26:19 +0200

    Update MauiProgram.cs

--- Planeedid/Planeedid/MauiProgram.cs
+++ Planeedid/Planeedid/MauiProgram.cs
@@ -9,9 +9,10 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("Montserrat-Bold.ttf", "MontserratBold");
+				fonts.AddFont("Montserrat-Semibold.ttf", "MontserratMedium");
+                fonts.AddFont("Montserrat-Medium.ttf", "MontserratRegular");
+            });
 
 		return builder.Build();
 	}
